ba6be350454d5f41809da13dbec733129b3cb96c
rannikkopyry <pyry.rannikko1@gmail.com>
2023-02-02 10:49:29 +0200

    Content changes

--- components/Homepage.tsx
+++ components/Homepage.tsx
@@ -8,9 +8,9 @@ export default function Homepage() {
         <div className='w-full grid grid-cols-1 md:grid-cols-[2fr_1fr] min-h-[500px] items-center max-w-7xl px-4 m-auto py-[60px] z-10'>
         <div className='md: pr-10'>
             <h1 className='text-black text-5xl md:text-8xl font-bold font-grotesk_bold'>
-                Sell more cars
+                Tool to make your social bio a powerful lead generator
             </h1>
-            <p className='text-black text-2xl mt-8 font-light'>Lorem ipsum adadadadadadadad</p>
+            <p className='text-black text-2xl mt-8 font-light'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto nemo error cupiditate ratione laudantium quia numquam incidunt deleniti maxime doloremque culpa, eius mollitia dolorum, dignissimos ab fugit est animi voluptas.</p>
         </div>
         <div className='flex-shrink-0 w-full items-center justify-center min-h-[615px] hidden md:flex'>
         <div className='min-h-[500px] flex items-center'>
@@ -30,7 +30,7 @@ export default function Homepage() {
         </div>
         </div> 
         <div className='mx-auto max-w-7xl pt-4 pb-12 px-4 sm:px-6 lg:px-8 opacity-50'>
-            <p className='text-black'>Trusted by:</p>
+            <p className='text-black'>Our clients:</p>
             <div className='grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5'>
                 <div className='col-span-1 flex justify-center md:col-span-2 lg:col-span-1'>
                     <img className='h-12 object-contain' src="./images/autolle.png" alt="" />
