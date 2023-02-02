de4d79c6d94beece0091c0096ab9e817fdb4d0b2
rannikkopyry <pyry.rannikko1@gmail.com>
2023-02-02 09:16:14 +0200

    Styling homepage

--- components/Homepage.tsx
+++ components/Homepage.tsx
@@ -3,11 +3,19 @@ import React from 'react'
 export default function Homepage() {
   return (
     <div className='min-h-screen bg-white'>
-        <div>
-            <h1 className='text-black'>
-            Motorlinks.io
+        <div className='w-full grid grid-cols-1 md:grid-cols-[2fr_1fr] min-h-[500px] items-center max-w-7xl px-4 m-auto py-[60px] z-10'>
+        <div className='md: pr-10'>
+            <h1 className='text-black text-5xl md:text-8xl font-bold font-grotesk_bold'>
+                Sell more cars
             </h1>
-            </div>
+            <p className='text-black text-2xl mt-8 font-light'>Lorem ipsum adadadadadadadad</p>
+        </div>
+        <div className='flex-shrink-0 w-full items-center justify-center min-h-[615px] hidden md:flex'>
+        <div className='min-h-[500px] flex items-center'>
+            <p className='text-black text-3xl mb-4 font-medium'>More leads</p>
+        </div>
+        </div>
+        </div> 
     </div>
   )
 }
