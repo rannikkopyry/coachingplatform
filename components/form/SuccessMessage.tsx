95a25c7d7c48afd372e813ed6e1d273ad822381b
rannikkopyry <pyry.rannikko1@gmail.com>
2023-02-07 10:30:52 +0200

    Styling for message

--- components/form/SuccessMessage.tsx
+++ components/form/SuccessMessage.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 function SuccessMessage() {
   return (
     <>
-      <div className="success-message">
-        <div className="text">
+      <div className="mt-8">
+        <div className="text-xl text-black">
           <h2>You are part of the waitlist!</h2>
           <p>We'll be sure to keep you updated on our launch.</p>
         </div>
