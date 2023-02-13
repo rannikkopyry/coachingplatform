2e7f84984ea51ab3b226b5900a32ae6db81473e4
rannikkopyry <pyry.rannikko1@gmail.com>
2023-02-13 13:19:18 +0200

    Protect dashboard from non auth users

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -3,7 +3,10 @@ import { getActiveProductsWithPrices } from 'utils/supabase-client';
 import { Product } from 'types';
 import AlternativeLayout from "components/AlternativeLayout"
 import { ReactElement } from 'react';
+import { withPageAuth } from '@supabase/auth-helpers-nextjs';
 
+
+export const getServerSideProps = withPageAuth({ redirectTo: '/signin' });
 
 export default function DashboardPage() {
   return <Dashboard />;
