631c60ce0164306d0dddd7c3f7408a538d688c97
rannikkopyry <pyry.rannikko1@gmail.com>
2023-02-13 11:03:33 +0200

    Styling admin navbar

--- components/ui/AdminNavbar/AdminNavbar.tsx
+++ components/ui/AdminNavbar/AdminNavbar.tsx
@@ -9,8 +9,8 @@ import { useSupabaseClient } from '@supabase/auth-helpers-react';
 const AdminNavbar = () => {
   const router = useRouter();
   const supabaseClient = useSupabaseClient();
-  const { user } = useUser();
-
+/*   const { user } = useUser();
+ */
   return (
     <nav className={s.root}>
       <a href="#skip" className="sr-only focus:not-sr-only">
@@ -36,7 +36,7 @@ const AdminNavbar = () => {
 
           <div className="hidden md:flex flex-1 justify-end space-x-8">
             <button className='invisible lg:visible md:visible max-h-[50px] mt-6 bg-black text-white text-base font-medium py-3 px-6 rounded-full cursor-pointer'>Coming soon</button>
-            {user ? (
+            {/* {user ? (
               <span
                 className={s.link}
                 onClick={async () => {
@@ -50,7 +50,7 @@ const AdminNavbar = () => {
               <Link href="/signin">
                 <a className={s.link}>Sign in</a>
               </Link>
-            )}
+            )} */}
           </div>
         </div>
       </div>
