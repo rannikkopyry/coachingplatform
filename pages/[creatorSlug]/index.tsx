1af70dd549a24b411a21d0fc747a80809d3cd8ed
rannikkopyry <pyry.rannikko1@gmail.com>
2023-02-20 13:00:48 +0200

    Install material tailwind

--- pages/[creatorSlug]/index.tsx
+++ pages/[creatorSlug]/index.tsx
@@ -8,6 +8,11 @@ import { useRouter } from 'next/router';
 import { supabase } from '@/utils/supabase-client';
 import SimpleLayout from 'components/SimpleLayout';
 import { ReactElement } from 'react';
+import {
+  Accordion,
+  AccordionHeader,
+  AccordionBody
+} from '@material-tailwind/react';
 
 interface Link {
   title: String;
@@ -323,6 +328,39 @@ const TreePage = () => {
                 </div>
               )}
             </div>
+            <Accordion open={open === 1}>
+              <AccordionHeader onClick={() => handleOpen(1)}>
+                What is Material Tailwind?
+              </AccordionHeader>
+              <AccordionBody>
+                We&apos;re not always in the position that we want to be at.
+                We&apos;re constantly growing. We&apos;re constantly making
+                mistakes. We&apos;re constantly trying to express ourselves and
+                actualize our dreams.
+              </AccordionBody>
+            </Accordion>
+            <Accordion open={open === 2}>
+              <AccordionHeader onClick={() => handleOpen(2)}>
+                How to use Material Tailwind?
+              </AccordionHeader>
+              <AccordionBody>
+                We&apos;re not always in the position that we want to be at.
+                We&apos;re constantly growing. We&apos;re constantly making
+                mistakes. We&apos;re constantly trying to express ourselves and
+                actualize our dreams.
+              </AccordionBody>
+            </Accordion>
+            <Accordion open={open === 3}>
+              <AccordionHeader onClick={() => handleOpen(3)}>
+                What can I do with Material Tailwind?
+              </AccordionHeader>
+              <AccordionBody>
+                We&apos;re not always in the position that we want to be at.
+                We&apos;re constantly growing. We&apos;re constantly making
+                mistakes. We&apos;re constantly trying to express ourselves and
+                actualize our dreams.
+              </AccordionBody>
+            </Accordion>
           </div>
         </div>
       </div>
