ab3e412c5ba5004486cef0295101d3c42ff09c5e
rannikkopyry <pyry.rannikko1@gmail.com>
2023-02-27 17:20:49 +0200

    Styling popup

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -9,17 +9,17 @@ const Modal = ({ isVisible, onClose }: Props) => {
   if (!isVisible) return null;
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-25 flex justify-center items-center">
+    <div className="w-auto p-4 fixed z-40 right-2 left-2 bottom-[70px] bg-white rounded-xl items-center border border-black text-center shadow-3xl">
       <div className="w-1/4">
         <button
           onClick={() => onClose()}
-          className="text-white flex flex-row place-self-end"
+          className=" text-black flex flex-row place-self-end"
         >
           X
         </button>
-        <div className="bg-white p-10 text-center">
+        <div className=" p-10 text-center">
           <h1 className="text-3xl mb-5">Subscribe to premium!</h1>
-          <p className="text-gray-400">First month for free</p>
+          <p className="text-black">First month for free</p>
         </div>
       </div>
     </div>
