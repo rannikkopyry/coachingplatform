f9ca6bbb4db3835c83c3a872a34aea5af62fead2
rannikkopyry <pyry.rannikko1@gmail.com>
2023-02-27 17:26:51 +0200

    Add new icon for contact bar

--- components/ContactBar.tsx
+++ components/ContactBar.tsx
@@ -8,7 +8,7 @@ function ContactBar() {
     <div className=" w-auto p-4 fixed z-40 right-2 left-2 bottom-2 bg-black rounded-full items-center border-b border-gray-500 text-center shadow-2xl">
       <Modal isVisible={showModal} onClose={() => setShowModal(false)} />
       <div className="flex items-center justify-center">
-        <img className="h-5 text-center" src="/phone.png" alt="" />
+        <img className="h-5 mr-2 text-center" src="/phone.svg" alt="" />
         <button onClick={() => setShowModal(true)} className="text-white">
           Contact me
         </button>
